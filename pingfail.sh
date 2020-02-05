cb2f6322eb628dde5091423e2e54e981bca7983e
Kenneth Bingham <w@qrk.us>
2020-02-05 18:53:21 -0500

    debug the pingfail script

--- pingfail.sh
+++ pingfail.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 set -e -u -o pipefail
 
@@ -8,7 +8,7 @@ until http --check-status GET elasticsearch:9200/_template/pingfail; do
         echo "ERROR: tried to PUT the index template"
         exit 1
     }
-    http --check-status --json --verbose PUT elasticsearch:9200/_template/pingfail < /index-template.json
+    http --json --verbose PUT elasticsearch:9200/_template/pingfail < /index-template.json
     let TEMPLATE_ATTEMPTS++
     sleep 1
 done
@@ -16,7 +16,14 @@ done
 
 while :; do
     ping -W1 -c1 ${PING_STRING:- -4 8.8.8.8} >/dev/null || {
-        http --check-status --json --verbose POST elasticsearch:9200/pingfail-$(date +%Y.%m.%d)/_doc/ ping:=false timestamp=$(( $(date +%s)*1000 ))
-    };
-    sleep 1;
+        http \
+            --check-status \
+            --verbose \
+            --ignore-stdin \
+            POST \
+            elasticsearch:9200/pingfail-$(date +%Y.%m.%d)/_doc/ \
+            ping:=false \
+            timestamp=$(( $(date +%s)*1000 ))
+    }
+    sleep 1
 done
