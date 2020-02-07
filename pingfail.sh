2e1df01bffda1a4ae86cf93b088f4633bc918a94
Kenneth Bingham <w@qrk.us>
2020-02-07 13:00:42 -0500

    write true on success

--- pingfail.sh
+++ pingfail.sh
@@ -3,28 +3,38 @@
 set -e -u -o pipefail
 
 TEMPLATE_ATTEMPTS=0
-until http --check-status GET elasticsearch:9200/_template/pingfail; do
+until http --check-status GET ${ELASTIC_URL:=elasticsearch:9200}/_template/pingfail; do
     [[ $TEMPLATE_ATTEMPTS -lt 9 ]] || {
         echo "ERROR: tried to PUT the index template"
         exit 1
     }
-    http --json --verbose PUT elasticsearch:9200/_template/pingfail < /index-template.json
+    http --json --verbose PUT elasticsearch:9200/_template/pingfail < ./index-template.json
     let TEMPLATE_ATTEMPTS++ || true
     sleep 1
 done
 
 
 while :; do
-    eval ping -W1 -c1 -${PING_IPV:=4} ${PING_TGT:=8.8.8.8} >/dev/null || {
+    if eval ping -W1 -c1 -${PING_IPV:=4} ${PING_TGT:=8.8.8.8} >/dev/null; then
         http \
             --check-status \
             --verbose \
             --ignore-stdin \
             POST \
-            elasticsearch:9200/pingfail-$(date +%Y.%m.%d)/_doc/ \
+            ${ELASTIC_URL}/pingfail-$(date +%Y.%m.%d)/_doc/ \
+            ping:=true \
+            target=${PING_TGT} \
+            timestamp=$(( $(date +%s)*1000 ))
+    else
+        http \
+            --check-status \
+            --verbose \
+            --ignore-stdin \
+            POST \
+            ${ELASTIC_URL}/pingfail-$(date +%Y.%m.%d)/_doc/ \
             ping:=false \
             target=${PING_TGT} \
             timestamp=$(( $(date +%s)*1000 ))
-    }
+    fi
     sleep 1
 done
